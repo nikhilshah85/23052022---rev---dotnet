a6bcdb564e07a04b87a1beefee478d77c76db64b
Nikhil <nikhil.shah2008@gmail.com>
2022-06-07 22:38:34 +0530

    Day 11 serialization demo

---
+++ Day 11/Serialization_Demo/Serialization_Demo/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Demo
+{
+    [Serializable] //this is called as an Attribute, which is an instruction to compiler and runtime
+                  
+    internal class Employee
+    {
+        public int empNo { get; set; }
+        public string empName { get; set; }
+        public string empDesigantion { get; set; }
+        public double empSalary { get; set; }
+        public bool empIsPermenant { get; set; }
+        public int empAvailableLeaves { get; set; }
+
+        public int ApplyLeave(int p_no_ofDays)
+        {
+            empAvailableLeaves = empAvailableLeaves - p_no_ofDays;
+            return empAvailableLeaves;
+        }
+
+    }
+}
---
+++ Day 11/Serialization_Demo/Serialization_Demo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace Serialization_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Binary Serialization
+            //Employee employee = new Employee();
+
+            //Console.WriteLine("Enter Employee No");
+            //employee.empNo =Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter Employee Name");
+            //employee.empName = Console.ReadLine();
+
+            //Console.WriteLine("Enter Employee Designation");
+            //employee.empDesigantion = Console.ReadLine();
+
+            //Console.WriteLine("Enter Employee Salary");
+            //employee.empSalary = Convert.ToDouble(Console.ReadLine());
+            ////employee.empSalary = Convert.ToDouble("1234.89");                
+
+            //Console.WriteLine("Enter Employee Is Permenant");
+            //employee.empIsPermenant = true;
+
+            //Console.WriteLine("Enter Employee Available Leaves");
+            //employee.empAvailableLeaves = 15;
+
+
+            //FileStream fs = new FileStream(@"C:\Users\hp\Desktop\Employee\" + employee.empNo + ".txt", FileMode.Create, FileAccess.Write);
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //formatter.Serialize(fs, employee);
+            //fs.Close();
+            //Console.WriteLine("Object Saved Successfully");
+            #endregion
+
+            #region Binary Deserialization 
+
+            //Console.WriteLine("Enter employee number : ");
+            //int no = Convert.ToInt32(Console.ReadLine());
+
+            //FileStream readObj = new FileStream(@"C:\Users\hp\Desktop\Employee\" + no + ".txt", FileMode.Open, FileAccess.Read);
+
+            //BinaryFormatter bfDeserialize = new BinaryFormatter();
+            //Employee empDetails = (Employee)bfDeserialize.Deserialize(readObj);
+
+            //// I have not created a new employee objet
+            //Console.WriteLine("Employee Number : " + empDetails.empNo);
+            //Console.WriteLine("Employee Name : " + empDetails.empName);
+            //Console.WriteLine("Employee Designation : " + empDetails.empDesigantion);
+            //Console.WriteLine("Employee Salary : " + empDetails.empSalary);
+            //Console.WriteLine("Employee Is Permenant " + empDetails.empIsPermenant);
+            //Console.WriteLine("Employee Available Leaves : " + empDetails.empAvailableLeaves);
+
+            readObj.Close();
+
+            #endregion
+
+        }
+    }
+}

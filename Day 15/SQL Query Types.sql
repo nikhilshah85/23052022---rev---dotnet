43c4bddb3c43b82b82062c849f871e9b4e3f4039
Nikhil <nikhil.shah2008@gmail.com>
2022-06-13 22:34:47 +0530

    SQL Intro

---
+++ Day 15/SQL Query Types.sql
@@ -0,0 +1,55 @@
+--write sql queries here
+--
+--select * from tbl_AccountDetails where accBalance > 8000
+
+
+
+
+
+
+
+
+
+
+--SQL Queries (Structured Query Language) - ISO standard
+	
+	
+	--1 --DDL - Data Defination Language - this deals with objects
+		--Create (to create a new Object, (table, view, procdure, function etc..)
+		--Alter (to modify any object, eg. add a new col to existing tbl, change datatype of a col)
+		--Drop (drop the object, delete the object from the database)
+    --2 --DML -Data Manipulation Language -- this deals with records
+		-- Insert -- add new account to acconts table
+		-- Update -- update an existing record
+		-- Delete -- delete an existing record
+	--3 -- DQL - Data Query Language - this deals with read the records
+		-- select --all the records, select limited records etc.
+		-- select with sort (order by clause)
+		-- select with filter (where clause)
+		-- select with grouping (group by clause)
+		-- select with Joins (select from 2 or more related tables)
+				--5 types of joins
+					-- inner join
+					-- left join
+					-- right join
+					-- full join
+					-- null join
+					-- self join
+		-- select with operators (wild cards operators, like, %, * etc)
+		-- select with functions
+	--4-- DCL - Data control languate
+		-- Grant - give permission
+		-- Revoke - take away permission
+	--5-- DTL - Data Transaction Language
+		-- Commit - (save)
+		-- Rollback - (undo)
+
+
+
+
+
+
+
+
+
+

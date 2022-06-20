0afdc16069b856f780b543db53071e118875b1ff
Nikhil <nikhil.shah2008@gmail.com>
2022-06-21 02:40:52 +0530

    Login Code for admin and user

---
+++ Day 20/Project/bankingAPP/bankingAPP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bankingLIB;
+namespace bankingAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables and Objects
+            Security secObj = new Security();
+            #endregion
+
+
+            Console.WriteLine("Welcome to Banking");
+            Console.WriteLine("Select Login Type");
+            Console.WriteLine("1. Admin");
+            Console.WriteLine("2. Customer");
+
+            int userType =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter User Name");
+            string uName = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            string pwd = Console.ReadLine();
+
+
+
+            if (userType == 1)
+            {
+                string isValidUser = secObj.Login(uName, pwd, UserType.Admin);               
+                if (isValidUser.Contains("Successful"))
+                {
+                    //show the admin menu here
+                }
+                else
+                {
+                    Console.WriteLine(isValidUser);
+                }
+
+     
+            }
+            else
+            {
+                string isValidUser = secObj.Login(uName, pwd, UserType.User);
+                if (isValidUser.Contains("Successful"))
+                {
+                    //show the user menu here
+                }
+                else
+                {
+                    Console.WriteLine(isValidUser);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Day 20/Project/bankingLIB/bankingLIB/Security.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace bankingLIB
+{
+   public enum UserType
+    {
+        Admin,
+        User
+    }
+    public class Security
+    {
+        public string Login(string userName, string password,UserType p_uType)
+        {
+            SqlConnection con = new SqlConnection("server=WIN8\\NIKHILINSTANCE;database=BankingDB;integrated security=true");
+            SqlCommand cmd;
+            if (p_uType == UserType.Admin)
+            {
+                cmd = new SqlCommand("select count(*) from AdminLogin where userName=@uName and password=@pwd",con);
+            }
+            else
+            {
+                cmd = new SqlCommand("select count(*) from userLogin where userName=@uName and password=@pwd", con);
+            }
+
+            cmd.Parameters.AddWithValue("@uName",userName);
+            cmd.Parameters.AddWithValue("@pwd", password);
+
+            con.Open();                    
+             int loginResult =(int) cmd.ExecuteScalar();
+            con.Close();
+
+            if (p_uType == UserType.User)
+            {
+                SqlCommand cmdAttemps = new SqlCommand("select status,attemps from userLogin where userName=@uName", con);
+
+                cmdAttemps.Parameters.AddWithValue("@uName", userName);
+                con.Open();
+
+                SqlDataReader readUser = cmdAttemps.ExecuteReader();
+                if (readUser.Read())
+                {
+                    string status = readUser[0].ToString();
+                    int attemps = (int)readUser[1];
+                    readUser.Close();
+
+                    if (status == "Blocked")
+                    {
+                        return "Account is Blocked, please contact Admin";
+                    }                    
+                    if(loginResult == 1 && status == "Active")
+                    {
+                        return "Login Successful";
+                    }                   
+                    else
+                    {
+                        SqlCommand updateAccount;
+                        if (attemps == 3)
+                        {
+                            updateAccount = new SqlCommand("update userLogin set status = 'Blocked' where userName=@uName", con);
+                        }
+                        else
+                        {
+                             updateAccount = new SqlCommand("update userLogin set attemps=attemps+1 where userName=@uName", con);
+                        }
+                        updateAccount.Parameters.AddWithValue("@uName", userName);
+                        updateAccount.ExecuteNonQuery();
+
+                        return "Login Failed for User " + userName;
+                    }
+                
+                }
+                con.Close();
+                return "Blank";
+            }
+            else
+            {
+                if (loginResult == 1)
+                {
+                    return "Login Successful For Admin";
+                }
+                else
+                {
+                    return "Login Failed For Admin";
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+

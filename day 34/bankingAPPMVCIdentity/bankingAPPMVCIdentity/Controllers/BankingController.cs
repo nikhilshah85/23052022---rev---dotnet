ad9d71eca1a2d9c6ab00f04b1ebb602d65e85737
Nikhil <nikhil.shah2008@gmail.com>
2022-07-22 23:18:45 +0530

    day 34 Identity framework

---
+++ day 34/bankingAPPMVCIdentity/bankingAPPMVCIdentity/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankingAPPMVCIdentity.Controllers
+{
+
+    [Authorize] //user will need to identify themself on a login page to get access to actions in this controller
+    public class BankingController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Banking()
+        {
+            return View();
+        }
+
+        public IActionResult TranferFunds()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadStatements()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
---
+++ day 34/bankingAPPMVCIdentity/bankingAPPMVCIdentity/Data/Migrations/20220722153744_newDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bankingAPPMVCIdentity.Data.Migrations
+{
+    public partial class newDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
